7581339f0d266dde5a4c13fc941ef4e9e6c2bcab
Achmad Dzulfian <achmad.dzulfian7@gmail.com>
2020-08-09 07:51:58 +0700

    Activate event

--- sw.js
+++ sw.js
@@ -9,3 +9,19 @@
 self.addEventListener('install', event => {
 	console.log('service worker has been installed');
 })
+
+
+/**
+ *
+ * Activate service worker
+ *
+ * If it doesn't triggered that's because we already have a service worker the previous version before we made the changes to it activating already in the browser.
+ *
+ * The browser doesn't automatically activate our new service worker version and replace the old ones with it
+ *
+ */
+self.addEventListener('activate', event => {
+	console.log('service worker has been activated');
+}) 
+
+ 
